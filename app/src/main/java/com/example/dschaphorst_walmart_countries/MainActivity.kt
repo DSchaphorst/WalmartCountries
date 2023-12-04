575b57bafd67016284e0efbe69a3df886c93477b
DSchaphorst <116312332+DSchaphorst@users.noreply.github.com>
2023-12-04 10:44:46 -0500

    Fix Gradel and Formatting

--- app/src/main/java/com/example/dschaphorst_walmart_countries/MainActivity.kt
+++ app/src/main/java/com/example/dschaphorst_walmart_countries/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.dschaphorst_walmart_countries
 
 import android.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dschaphorst_walmart_countries.databinding.ActivityMainBinding
 import com.example.dschaphorst_walmart_countries.util.UIState
+
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -45,6 +51,16 @@ class MainActivity : AppCompatActivity() {
                     binding.loadingSpinner.visibility = View.GONE
                     binding.homeRecycle.visibility = View.VISIBLE
                     mainAdapter.setData(state.countries)
+
+                    recyclerView = findViewById(R.id.home_recycle)
+                    val layoutManager = LinearLayoutManager(this)
+                    recyclerView.layoutManager = layoutManager
+                    val dividerItemDecoration = DividerItemDecoration(
+                        recyclerView.context,
+                        layoutManager.orientation
+                    )
+                    recyclerView.addItemDecoration(dividerItemDecoration)
+                    recyclerView.adapter = mainAdapter
                 }
                 is UIState.ERROR -> {
                     Log.e(TAG, "Error Loading network data.")
